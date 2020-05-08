53379e74c052d28daa2b1edfe5303a8ff5125e0f
suhasghorp <suhasghorp@gmail.com>
2020-05-08 14:57:55 -0400

    Second Commit

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ fn main() {
     let mut duration = start.elapsed();
     println!("Non-parallel Price is {}, took {:?}", price, duration);
 
+    println!("Number of logical cores is {}", num_cpus::get());
     start = Instant::now();
     price = pricer::MCEuroOptPricer::new(200.0,200.0,0.15,0.1,1.0,pricer::OptionType::Call,365,100000,true,0, 1).compute_price();
     duration = start.elapsed();

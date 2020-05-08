23685e56519c3ecffc12016a45c95ee79953dc00
suhasghorp <suhasghorp@gmail.com>
2020-05-08 15:32:40 -0400

    Committed

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,15 @@ use std::time::Instant;
 
 fn main() {
     let mut start = Instant::now();
-    let mut price = pricer::MCEuroOptPricer::new(200.0,200.0,0.15,0.1,1.0,pricer::OptionType::Call,365,100000,false,0, 1).compute_price();
+    let mut price = pricer::MCEuroOptPricer::new(200.0,200.0,0.15,0.1,1.0,pricer::OptionType::Call,365,1000000,false,0, 1).compute_price();
     let mut duration = start.elapsed();
-    println!("Non-parallel Price is {}, took {:?}", price, duration);
+    println!("Non-parallel Option Price is {}, took {:?}", price, duration);
 
     println!("Number of logical cores is {}", num_cpus::get());
+    println!("Number of Scenarios: 1,000,000");
     start = Instant::now();
-    price = pricer::MCEuroOptPricer::new(200.0,200.0,0.15,0.1,1.0,pricer::OptionType::Call,365,100000,true,0, 1).compute_price();
+    price = pricer::MCEuroOptPricer::new(200.0,200.0,0.15,0.1,1.0,pricer::OptionType::Call,365,1000000,true,0, 1).compute_price();
     duration = start.elapsed();
-    println!("Parallel Price is {}, took {:?}", price, duration);
+    println!("Parallel Option Price is {}, took {:?}", price, duration);
 }
 
